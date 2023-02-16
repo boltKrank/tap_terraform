8beb9d555d19d8fb369f4d9c0d9de2fe75af4274
Simon Anderson <simon.anderson111@gmail.com>
2023-02-16 18:12:40 +1100

    Profile update

--- Azure/outputs.tf
+++ Azure/outputs.tf
@@ -6,13 +6,13 @@ output "outgoing_view_ip" {
   value = data.azurerm_public_ip.tap_view_PIP.ip_address
 }
 
-output "outgoing_build_ip" {
-  value = data.azurerm_public_ip.tap_build_PIP.ip_address
-}
+# output "outgoing_build_ip" {
+#   value = data.azurerm_public_ip.tap_build_PIP.ip_address
+# }
 
-output "outgoing_run_ip" {
-  value = data.azurerm_public_ip.tap_run_PIP.ip_address
-}
+# output "outgoing_run_ip" {
+#   value = data.azurerm_public_ip.tap_run_PIP.ip_address
+# }
 
 output "public_ip_address" {
   value = azurerm_public_ip.bootstrap_pip.ip_address
