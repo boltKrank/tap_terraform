01f5cafc0be9b7bfb8bbdb06ee2d55f9d84f5142
Simon Anderson <simon.anderson111@gmail.com>
2023-02-23 14:48:05 +1100

    Fixing up session recording

--- Azure/outputs.tf
+++ Azure/outputs.tf
@@ -6,6 +6,10 @@ output "bootstrap_public_ip_address" {
   value = azurerm_public_ip.bootstrap_pip.ip_address
 }
 
+output "ACR admin password" {
+  value = nonsensitive(azurerm_container_registry.tap_acr.admin_password)
+}
+
 # output "tls_private_key" {
 #   value     = tls_private_key.example_ssh.private_key_pem
 #   sensitive = true
