486047a2b8fa4f1315ffe22bf91b477686e67eb7
Simon Anderson <simon.anderson111@gmail.com>
2023-02-27 16:01:38 +1100

    updated counts

--- AWS/main.tf
+++ AWS/main.tf
@@ -1,1 +1,19 @@
 # AWS TAP Install
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.location
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
---
+++ AWS/variables.tf
@@ -0,0 +1,3 @@
+variable "location" {
+  value = "Instance location"
+}
