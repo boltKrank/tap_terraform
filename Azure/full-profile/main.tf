7a6bfe4ae08232e28a9883be2b58f9c278f0fd42
Simon Anderson <simon.anderson111@gmail.com>
2023-05-17 22:57:30 +1000

    Updated for TAP 1.5.1

--- Azure/full-profile/main.tf
+++ Azure/full-profile/main.tf
@@ -4,11 +4,11 @@ provider "azurerm" {
   features {}
 
 
-  # subscription_id = var.subscription_id
+  subscription_id = var.subscription_id
   # tap_sp
-  # client_id       = var.sp_client_id
-  # client_secret   = var.sp_secret
-  # tenant_id       = var.sp_tenant_id
+  client_id       = var.sp_client_id
+  client_secret   = var.sp_secret
+  tenant_id       = var.sp_tenant_id
 
 }
 
@@ -176,27 +176,29 @@ resource "azurerm_linux_virtual_machine" "main" {
     destination = "/home/${var.bootstrap_username}/" 
   }
 
-
-  # Seperate shell so user inherits the groupadd in the following shell to run docker as non-root
+  # Install kubectl
   provisioner "remote-exec" {
     inline = [
-      "curl -fsSL https://get.docker.com -o get-docker.sh",
-      "sudo sh get-docker.sh",
-      "sudo groupadd docker",
-      "sudo usermod -aG docker $USER",
-      "echo 'END DOCKER INSTALL'",
+      "curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl",
+      "sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl",      
     ]
-
   }
-  
 
-  # # Run commands:
-  provisioner "remote-exec" { 
-
-    # Pre-reqs: Azure CLI, Pivnet CLI, Tanzu CLI, and Cluster essentials for all clusters
+  # Install Azure CLI and set k8s context
+  provisioner "remote-exec" {
     inline = [
       "curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash",   
       "az login --service-principal -u ${var.sp_client_id} -p ${var.sp_secret} --tenant ${var.sp_tenant_id}",
+      "az aks get-credentials --resource-group ${var.tap_full_aks_name} --name ${var.tap_full_aks_name} --admin --overwrite-existing",      
+      "kubectl config get-contexts",
+      "kubectl config use-context ${var.tap_full_aks_name}-admin",
+      "kubectl create ns tap-install", 
+    ]
+  }
+
+  # Install Pivnet and Tanzu CLI
+  provisioner "remote-exec" {
+     inline = [
       "wget https://github.com/pivotal-cf/pivnet-cli/releases/download/v${var.pivnet_version}/pivnet-linux-amd64-${var.pivnet_version}",
       "chmod 755 pivnet-linux-amd64-${var.pivnet_version}",
       "sudo mv pivnet-linux-amd64-${var.pivnet_version} /usr/local/bin/pivnet",
@@ -207,56 +209,154 @@ resource "azurerm_linux_virtual_machine" "main" {
       "export TANZU_CLI_NO_INIT=true",
       "tanzu version",
       "tanzu plugin install --local cli all",
-      "rm -f tanzu-framework-*-amd64-*.tar",
-      "curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl",
-      "sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl",      
-      "docker login ${var.tap_acr_name}.azurecr.io -u ${var.tap_acr_name} -p ${local.acr_pass}",
-      "docker login ${var.tanzu_registry_hostname} -u ${var.tanzu_registry_username} -p ${var.tanzu_registry_password}",  
+      "rm -f tanzu-framework-*-amd64-*.tar",      
+     ]
+  }
+
+  # Install cluster essentials and kapp and imgpkg
+  provisioner "remote-exec" {
+     inline = [
       "cd",      
       "pivnet download-product-files --product-slug='tanzu-cluster-essentials' --release-version='${var.cluster_essentials_version}' --glob='tanzu-cluster-essentials-linux-amd64-*'",
       "mkdir tanzu-cluster-essentials",
       "tar xzvf tanzu-cluster-essentials-*-amd64-*.tgz -C tanzu-cluster-essentials",
-      "export INSTALL_BUNDLE=${var.tap_acr_name}.azurecr.io/tanzu-cluster-essentials/cluster-essentials-bundle:${var.cluster_essentials_version}",
-      "export INSTALL_REGISTRY_HOSTNAME=${var.tap_acr_name}.azurecr.io",
-      "export INSTALL_REGISTRY_USERNAME=${var.tap_acr_name}",
-      "export INSTALL_REGISTRY_PASSWORD=${local.acr_pass}",
-      "cd tanzu-cluster-essentials",           
-      "az aks get-credentials --resource-group ${var.tap_full_aks_name} --name ${var.tap_full_aks_name} --admin --overwrite-existing",      
-      "kubectl config get-contexts",
-      "kubectl config use-context ${var.tap_full_aks_name}-admin",
-      "kubectl create ns tap-install",    
+      "export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:79abddbc3b49b44fc368fede0dab93c266ff7c1fe305e2d555ed52d00361b446",
+      "export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com",
+      "export INSTALL_REGISTRY_USERNAME=${var.tanzu_registry_username}",
+      "export INSTALL_REGISTRY_PASSWORD=${var.tanzu_registry_password}",
+      "cd $HOME/tanzu-cluster-essentials",           
       "./install.sh --yes",
-      "tanzu secret registry add tap-registry --username \"${var.tap_acr_name}\" --password \"${local.acr_pass}\" --server ${var.tap_acr_name}.azurecr.io --export-to-all-namespaces --yes --namespace tap-install", 
-      "tanzu secret registry add kp-default-repository-creds --username \"${var.tap_acr_name}\" --password \"${local.acr_pass}\" --server ${var.tap_acr_name}.azurecr.io --export-to-all-namespaces --yes --namespace tap-install",
-      "tanzu package repository add tanzu-tap-repository --url ${var.tap_acr_name}.azurecr.io/tanzu-application-platform/tap-packages:${var.tap_version} --namespace tap-install",   
-      "tanzu package repository get tanzu-tap-repository --namespace tap-install",
-      "tanzu package repository add tbs-full-deps-repository --url ${var.tap_acr_name}.azurecr.io/tanzu-application-platform/full-tbs-deps-package-repo:${var.tbs_version} --namespace tap-install",   
-      "tanzu package repository get tbs-full-deps-repository --namespace tap-install",
-      "tanzu package available list --namespace tap-install",
-      "cd",
-      "mkdir -p overlays",         
-      "kubectl config use-context ${var.tap_full_aks_name}-admin",
-      "export ACR_NAME=${var.tap_acr_name}",
-      "export ACR_PASS=${local.acr_pass}",
-      "export ACR_SERVER=$ACR_NAME.azure.io",
-      "export ENVOY_IP=${azurerm_public_ip.tap-full-pip.ip_address}",
-      "export DOMAIN_NAME=${var.tap_full_dns_prefix}.$(echo $ENVOY_IP | sed 's/\\./-/g').${var.domain_name}",
-      "export TAP_RG=${azurerm_resource_group.tap_resource_group.name}",
-      "cd",
-      "docker run --rm -v $PWD/certs:/certs hitch openssl req -new -nodes -out /certs/ca.csr -keyout /certs/ca.key -subj \"/CN=default-ca/O=TAP/C=AU\"",
-      "sudo chown $USER:$USER certs/*",
-      "chmod og-rwx certs/ca.key",
-      "docker run --rm -v $PWD/certs:/certs hitch openssl x509 -req -in /certs/ca.csr -days 3650 -extfile /etc/ssl/openssl.cnf -extensions v3_ca -signkey /certs/ca.key -out /certs/ca.crt",  
-      "cat certs/ca.crt | sed 's/^/    /g' > tls-cert-sed.txt",
-      "cat certs/ca.key | sed 's/^/    /g' > tls-key-sed.txt",  
-      "ls",
-      "chmod 755 create-contour-default-tls.sh create-cnrs-https.sh create-cnrs-slim.sh create-metadata-store-ingress-tls.sh create-tap-values.sh create-tap-values-full.sh tap-install.sh",
-      "./create-contour-default-tls.sh; ./create-cnrs-https.sh; ./create-metadata-store-ingress-tls.sh; ./create-tap-values.sh; ./create-tap-values-full.sh",
-      "cat tap-values.yaml",    
-      "cat tap-values-full.yaml",
-      "cd",      
+      "sudo cp $HOME/tanzu-cluster-essentials/kapp /usr/local/bin/kapp",
+      "sudo cp $HOME/tanzu-cluster-essentials/imgpkg /usr/local/bin/imgpkg",
     ]
   }
+
+  # Relocate images
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "export IMGPKG_REGISTRY_HOSTNAME_0=registry.tanzu.vmware.com",
+  #     "export IMGPKG_REGISTRY_USERNAME_0=${var.tanzu_registry_username}",
+  #     "export IMGPKG_REGISTRY_PASSWORD_0=${var.tanzu_registry_password}",
+  #     "export IMGPKG_REGISTRY_HOSTNAME_1=${var.tap_acr_name}.azurecr.io",
+  #     "export IMGPKG_REGISTRY_USERNAME_1=${var.tap_acr_name}",
+  #     "export IMGPKG_REGISTRY_PASSWORD_1=${local.acr_pass}",
+  #     "export INSTALL_REGISTRY_USERNAME=${var.tap_acr_name}",
+  #     "export INSTALL_REGISTRY_PASSWORD=${local.acr_pass}",
+  #     "export INSTALL_REGISTRY_HOSTNAME=${var.tap_acr_name}.azurecr.io",
+  #     "export TAP_VERSION=${var.tap_version}",
+  #     "export INSTALL_REPO=tanzu-application-platform", 
+  #     "imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/tap-packages:${var.tap_version} --to-repo ${var.tap_acr_name}.azurecr.io/tanzu-application-platform/tap-packages",   
+  #     "imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/full-tbs-deps-package-repo:${var.tbs_version} --to-repo ${var.tap_acr_name}.azurecr.io/tanzu-application-platform/tbs-full-deps",
+  #   ]
+  # }
+
+  # "imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/tap-packages:${var.tap_version} --to-repo ${var.tap_acr_name}.azurecr.io/tanzu-application-platform/tap-packages",
+  # imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/full-tbs-deps-package-repo:${var.tbs_version} --to-repo ${var.tap_acr_name}.azurecr.io/tbs-full-deps
+
+
+  # Add the package repository
+  provisioner "remote-exec" {
+    inline = [
+      "export INSTALL_REGISTRY_USERNAME=${var.tap_acr_name}",
+      "export INSTALL_REGISTRY_PASSWORD=${local.acr_pass}",
+      "export INSTALL_REGISTRY_HOSTNAME=${var.tap_acr_name}.azurecr.io",
+      "export TAP_VERSION=${var.tap_version}",      
+      "tanzu secret registry add tap-registry --username ${var.tap_acr_name} --password ${local.acr_pass} --server ${var.tap_acr_name}.azurecr.io --export-to-all-namespaces --yes --namespace tap-install",
+      "tanzu secret registry add registry-credentials --server ${var.tap_acr_name}.azurecr.io --username ${var.tap_acr_name} --password ${local.acr_pass} --export-to-all-namespaces --yes",      
+      "tanzu package repository add tanzu-tap-repository --url ${var.tap_acr_name}.azurecr.io/tanzu-application-platform/tap-packages:${var.tap_version} --namespace tap-install",
+      "tanzu package repository add tbs-full-deps-repository --url ${var.tap_acr_name}.azurecr.io/tanzu-application-platform/tbs-full-deps:${var.tbs_version}--namespace tap-install",
+      "tanzu package repository get tanzu-tap-repository --namespace tap-install",
+      "tanzu package available list --namespace tap-install",
+    ]
+  }
+}  
+
+  # Seperate shell so user inherits the groupadd in the following shell to run docker as non-root
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "curl -fsSL https://get.docker.com -o get-docker.sh",
+  #     "sudo sh get-docker.sh",
+  #     "sudo groupadd docker",
+  #     "sudo usermod -aG docker $USER",
+  #     "echo 'END DOCKER INSTALL'",
+  #   ]
+  # }
+
+
+  # install TAP
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "export TAP_VERSION=${var.tap_version}",    
+  #     "tanzu package install tap -p tap.tanzu.vmware.com -v $TAP_VERSION --values-file tap-values.yaml -n tap-install",
+  #   ]
+  # }
+
+
+  # TODO: https://docs.vmware.com/en/VMware-Tanzu-Application-Platform/1.5/tap/install-online-profile.html
+
+
+  # "imgpkg copy -b ${var.tanzu_registry_hostname}/tanzu-application-platform/tap-packages:${var.tap_version} --to-repo ${var.tap_acr_name}.azurecr.io/tanzu-application-platform/tap-packages --include-non-distributable-layers",             
+  
+  # exec
+  # provisioner "remote-exec" {
+  #   inline = [
+      
+  #   ]
+  # }
+
+
+
+  # # Pre-reqs: Azure CLI, Cluster essentials for all clusters
+  # provisioner "remote-exec" {     
+  #   inline = [
+
+  #     "docker login ${var.tap_acr_name}.azurecr.io -u ${var.tap_acr_name} -p ${local.acr_pass}",
+  #     "docker login ${var.tanzu_registry_hostname} -u ${var.tanzu_registry_username} -p ${var.tanzu_registry_password}",  
+      
+
+  #   ]
+  # }
+
+
+
+  # provisioner "remote-exec" {
+  #   inline = [ 
+   
+  #     "imgpkg copy -b ${var.tanzu_registry_hostname}/tanzu-application-platform/tap-packages:${var.tap_version} --to-repo ${var.tap_acr_name}.azurecr.io/tanzu-application-platform/tap-packages --include-non-distributable-layers",             
+  #     "cd",
+  #     "tanzu secret registry add tap-registry --username \"${var.tap_acr_name}\" --password \"${local.acr_pass}\" --server ${var.tap_acr_name}.azurecr.io --export-to-all-namespaces --yes --namespace tap-install", 
+  #     "tanzu secret registry add kp-default-repository-creds --username \"${var.tap_acr_name}\" --password \"${local.acr_pass}\" --server ${var.tap_acr_name}.azurecr.io --export-to-all-namespaces --yes --namespace tap-install",
+  #     "tanzu package repository add tanzu-tap-repository --url ${var.tap_acr_name}.azurecr.io/tanzu-application-platform/tap-packages:${var.tap_version} --namespace tap-install",   
+  #     "tanzu package repository get tanzu-tap-repository --namespace tap-install",
+  #     "tanzu package repository add tbs-full-deps-repository --url ${var.tap_acr_name}.azurecr.io/tanzu-application-platform/full-tbs-deps-package-repo:${var.tbs_version} --namespace tap-install",   
+  #     "tanzu package repository get tbs-full-deps-repository --namespace tap-install",
+  #     "tanzu package available list --namespace tap-install",
+  #     "cd",
+  #     "mkdir -p overlays",         
+  #     "kubectl config use-context ${var.tap_full_aks_name}-admin",
+  #     "export ACR_NAME=${var.tap_acr_name}",
+  #     "export ACR_PASS=${local.acr_pass}",
+  #     "export ACR_SERVER=$ACR_NAME.azure.io",
+  #     "export ENVOY_IP=${azurerm_public_ip.tap-full-pip.ip_address}",
+  #     "export DOMAIN_NAME=${var.tap_full_dns_prefix}.$(echo $ENVOY_IP | sed 's/\\./-/g').${var.domain_name}",
+  #     "export TAP_RG=${azurerm_resource_group.tap_resource_group.name}",
+  #     "cd",
+  #     "docker run --rm -v $PWD/certs:/certs hitch openssl req -new -nodes -out /certs/ca.csr -keyout /certs/ca.key -subj \"/CN=default-ca/O=TAP/C=AU\"",
+  #     "sudo chown $USER:$USER certs/*",
+  #     "chmod og-rwx certs/ca.key",
+  #     "docker run --rm -v $PWD/certs:/certs hitch openssl x509 -req -in /certs/ca.csr -days 3650 -extfile /etc/ssl/openssl.cnf -extensions v3_ca -signkey /certs/ca.key -out /certs/ca.crt",  
+  #     "cat certs/ca.crt | sed 's/^/    /g' > tls-cert-sed.txt",
+  #     "cat certs/ca.key | sed 's/^/    /g' > tls-key-sed.txt",  
+  #     "ls",
+  #     "chmod 755 create-contour-default-tls.sh create-cnrs-https.sh create-cnrs-slim.sh create-metadata-store-ingress-tls.sh create-tap-values.sh create-tap-values-full.sh tap-install.sh",
+  #     "./create-contour-default-tls.sh; ./create-cnrs-https.sh; ./create-metadata-store-ingress-tls.sh; ./create-tap-values.sh; ./create-tap-values-full.sh",
+  #     "cat tap-values.yaml",    
+  #     "cat tap-values-full.yaml",
+  #     "cd",      
+  #    ]    
+  # }
+    
+
 
   #  "tanzu package install tap -p tap.tanzu.vmware.com -v ${var.tap_version} --values-file tap-values.yaml -n tap-install",
 
@@ -294,6 +394,6 @@ resource "azurerm_linux_virtual_machine" "main" {
       # "tanzu package installed update -n tap-install tap -f tap-values-view.yaml --poll-timeout 20m",
       # "kubectl get httpproxy -A",   
 
-}  
+
 
  
